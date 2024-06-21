1cb08c3a7d28daa7aba93a1aa8e9d4f303e39f74
User <shy_11@korea.ac.kr>
2024-06-21 07:06:34 +0000

    업데이트

--- 프로그래머스/2단계/N개의 최소공배수.py
+++ 프로그래머스/2단계/N개의 최소공배수.py
@@ -1,3 +1,11 @@
 # N개의 최소 공배수
 # https://school.programmers.co.kr/learn/courses/30/lessons/12953
 
+import math
+
+def solution(arr):
+    answer = arr[0]
+    for i in arr:
+        answer = (i * answer) // math.gcd(i, answer)
+
+    return answer
